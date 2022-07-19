c2d1c31ce65986a94d5c9521f2ec673449eccdfc
Le Thanh An <an@stashaway.com>
2022-07-19 22:21:08 +0800

    add dev tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Slides from './Slides';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Demos from './Demos';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -14,6 +15,7 @@ function App() {
         <div style={{ padding: '1rem' }}>
           <Demos />
         </div>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Router>
   );
